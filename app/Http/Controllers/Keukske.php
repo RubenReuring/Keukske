f0089ff045fe633ebc51c17ddd99719df5cfe691
rubenmachiels <ruben@reuring.studio>
2023-03-07 20:42:02 +0100

    Controller edit

--- app/Http/Controllers/Keukske.php
+++ app/Http/Controllers/Keukske.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class Keukske extends Controller
 {
     public function matchCode(Request $request) {
-       return($request->Naam);
+        $validated = $request->validate([
+            'Naam' => 'required',
+            'Email' => 'required',
+            'Code' => 'required'
+        ]);
+
+        return($request->Naam);
     }
 }
