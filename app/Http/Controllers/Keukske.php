a9a7e42e81a0e8dca58d4b5ab285846d7f8ffa9f
rubenmachiels <ruben@reuring.studio>
2023-03-07 20:40:39 +0100

    Controller edit

--- app/Http/Controllers/Keukske.php
+++ app/Http/Controllers/Keukske.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class Keukske extends Controller
 {
     public function matchCode(Request $request) {
-       return($request);
+       return($request->Naam);
     }
 }
