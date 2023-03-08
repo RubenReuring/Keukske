1af1310dcc9d5a547d4d0902e0fb965471ce9908
rubenmachiels <ruben@reuring.studio>
2023-03-08 15:46:33 +0100

    Controller edit

--- app/Http/Controllers/Keukske.php
+++ app/Http/Controllers/Keukske.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class Keukske extends Controller
 {
     public function matchCode(Request $request) {
-        $totalCode = strtoupper($tempCode= $request->code1 . $request->code2 . $request->code3 . $request->code4);
+        //$totalCode = strtoupper($tempCode= $request->code1 . $request->code2 . $request->code3 . $request->code4);
+        $totalCode = '1b4y';
         $codeMatch = DB::table('keukske_unique_codes')->where('uniqueCode', $totalCode)->value('id');
 
         dd($codeMatch);
