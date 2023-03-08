15df28e044420627074ecb0c16d6ed77919e24ab
rubenmachiels <ruben@reuring.studio>
2023-03-08 15:44:59 +0100

    Controller edit

--- app/Http/Controllers/Keukske.php
+++ app/Http/Controllers/Keukske.php
@@ -7,18 +7,10 @@ use Illuminate\Http\Request;
 class Keukske extends Controller
 {
     public function matchCode(Request $request) {
-        $validator = $request->validate([
-            'naam' => 'required',
-            'mail' => 'required',
-            'code1' => 'required',
-            'code2' => 'required',
-            'code3' => 'required',
-            'code4' => 'required',
-        ]);
-
         $totalCode = strtoupper($tempCode= $request->code1 . $request->code2 . $request->code3 . $request->code4);
         $codeMatch = DB::table('keukske_unique_codes')->where('uniqueCode', $totalCode)->value('id');
 
-        return response()->json(['formatCode'=>$codeMatch]);
+        dd($codeMatch);
+//        return response()->json(['formatCode'=>$codeMatch]);
     }
 }
