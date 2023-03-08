a80ef8a846315167243826e44230a48b0e7d2d5d
rubenmachiels <ruben@reuring.studio>
2023-03-08 12:33:43 +0100

    Controller edit

--- public/js/keukske.js
+++ public/js/keukske.js
@@ -1,22 +1,22 @@
-console.log('hello')
+
 
 $('.test').on('click', function(){
-
-    $.ajax({
-        url: 'http://164.92.134.186/keukske-actie',
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            naam: 'ruben',
-            mail: 'ruben@reuring.studio',
-            code: ''
-        },
-        success: function(result){
-            console.log(result)
-        },
-        error: function(result){
-            console.log(result.errors)
-        }
-    })
+    console.log('hello')
+    // $.ajax({
+    //     url: 'http://164.92.134.186/keukske-actie',
+    //     method: 'GET',
+    //     dataType: 'json',
+    //     data: {
+    //         naam: 'ruben',
+    //         mail: 'ruben@reuring.studio',
+    //         code: ''
+    //     },
+    //     success: function(result){
+    //         console.log(result)
+    //     },
+    //     error: function(result){
+    //         console.log(result.errors)
+    //     }
+    // })
 
 })
