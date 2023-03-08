6dd2af145e4f5f17f349c02e7a25b0a064353716
rubenmachiels <ruben@reuring.studio>
2023-03-08 12:45:55 +0100

    Controller edit

--- app/Http/Controllers/Keukske.php
+++ app/Http/Controllers/Keukske.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 class Keukske extends Controller
 {
     public function matchCode(Request $request) {
-        $validator = $request->validateWithBag('login', [
+        $validator = $request->validate([
             'naam' => 'required',
             'mail' => 'required',
             'code' => 'required'
         ]);
 
-        return response()->json(($validator)->withErrors($validator, 'login'));
+        return response()->json($validator);
     }
 }
