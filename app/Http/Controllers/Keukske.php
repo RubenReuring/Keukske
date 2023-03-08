2aab3a343ce65865ee09c46a446ce7a420c21dd3
rubenmachiels <ruben@reuring.studio>
2023-03-08 11:39:44 +0100

    Controller edit

--- app/Http/Controllers/Keukske.php
+++ app/Http/Controllers/Keukske.php
@@ -13,6 +13,6 @@ class Keukske extends Controller
             'code' => 'required'
         ]);
 
-        return redirect('welcome')->withErrors($validator, 'code');
+        return redirect('/')->withErrors($validator, 'code');
     }
 }
