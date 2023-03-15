07ab15f30fca05805c07f1bcedfc594c8c3c32fa
rubenmachiels <ruben@reuring.studio>
2023-03-15 22:17:56 +0100

    Controller edit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/keukske-actie', [\App\Http\Controllers\Keukske::class, 'matchCode']);
