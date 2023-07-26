cefa16d88cd206541780ffca3e20650f45e30d4f
rubenmachiels <ruben@reuring.studio>
2023-07-26 16:55:47 +0200

    Controller edit

--- resources/views/spherestory/flexibility-limburg.blade.php
+++ resources/views/spherestory/flexibility-limburg.blade.php
@@ -18,8 +18,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
     <script type="text/javascript">WebFont.load({  google: {    families: ["Inconsolata:400,700"]  }});</script>
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-    <link href="/assets/flexibility-limburg/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
-    <link href="/assets/flexibility-limburg/images/webclip.png" rel="apple-touch-icon">
+    <link href="/spherestory/assets/flexibility-limburg/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="/spherestory/assets/flexibility-limburg/images/webclip.png" rel="apple-touch-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css">
     <link href="https://cdn.jsdelivr.net/npm/video.js@8.3.0/dist/video-js.min.css" rel="stylesheet">
 </head>
