f5fbdb8b1e0552e0a388a901ea6816a0c4582ca9
rubenmachiels <ruben@reuring.studio>
2023-07-26 16:31:53 +0200

    Controller edit

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/ama-group/2K23-QR-001', function () { return redirect('https://www.
 Route::get('/ama-group/2K23-QR-002', function () { return redirect('https://www.ama-group.info/ama-projecten/dotterbloem-10'); });
 
 // 360 Experience
-Route::get('/spherestory/flexibility-limburg', function () { return view('/spherestory/flexbility-limburg'); });
+Route::get('/spherestory/flexibility-limburg', function () { return view('spherestory/flexbility-limburg'); });
