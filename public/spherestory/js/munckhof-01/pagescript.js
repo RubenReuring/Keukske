0ea414e6ae3c4f16cff41eef1472764c6c6d9166
rubenmachiels <ruben@reuring.studio>
2023-08-14 11:55:16 +0200

    Controller edit

--- public/spherestory/js/munckhof-01/pagescript.js
+++ public/spherestory/js/munckhof-01/pagescript.js
@@ -84,16 +84,16 @@ let viewer = pannellum.viewer("panorama", {
                     cssClass: "custom-hotspot sphere-hotspot_company"
                 },
                 {
-                    pitch: -3.51,
-                    yaw: -31.2,
+                    pitch: 0.4,
+                    yaw: 81.29,
                     type: "info",
                     cssClass: "custom-hotspot flexibility-hotspot_video-1",
                     clickHandlerFunc: hotspotAction,
                     clickHandlerArgs: {hotspot: "sphere-hotspot_video-1", type: "video-modal"}
                 },
                 {
-                    pitch: 0.95,
-                    yaw: 129,
+                    pitch: -3.51,
+                    yaw: -31.2,
                     type: "info",
                     cssClass: "custom-hotspot flexibility-hotspot_video-2",
                     clickHandlerFunc: hotspotAction,
