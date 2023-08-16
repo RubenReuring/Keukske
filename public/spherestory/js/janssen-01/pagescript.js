9d95b937046ac0c9a196b09a3f81b2c1f52a999f
rubenmachiels <ruben@reuring.studio>
2023-08-16 19:29:14 +0200

    Controller edit

---
+++ public/spherestory/js/janssen-01/pagescript.js
@@ -0,0 +1,125 @@
+
+function hotspotAction(e, data) {
+    let currentHotspot = data.hotspot;
+    let currentType = data.type;
+    if(currentType === 'video-modal'){
+        $('.room-videos').removeClass('inactive');
+        $('.room-videos').addClass('active');
+        setTimeout(function(){
+            $('.room-videos').css('opacity', '1');
+        },50)
+        $('._360videocontainer.modal-active').removeClass('modal-active');
+        $('._360videocontainer').each(function(){
+            if($(this).find('.video-js').attr('id') === currentHotspot){
+                $(this).addClass('modal-active').trigger('classChange');
+            }
+        })
+    }
+}
+
+$('.video-js').each(function(){
+    var player = videojs(this);
+    let currentPlayer = $(this);
+    $(currentPlayer).parents("._360videocontainer").find("._360vc-cb.play, ._360vc-cb.pause").on('click', function () {
+        if(!player.paused()){
+            player.pause();
+            $('._360vc-cb.play').removeClass('videocontrolinactive')
+            $('._360vc-cb.pause').addClass('videocontrolinactive')
+        }
+        else {
+            player.play();
+            $(this).parents("._360videocontainer").find('._360vc-cb.play').addClass('videocontrolinactive')
+            $(this).parents("._360videocontainer").find('._360vc-cb.pause').removeClass('videocontrolinactive')
+        }
+    });
+    $(currentPlayer).parents("._360videocontainer").find("._360vc_close").on('click', function(){
+        player.currentTime(0);
+        player.pause();
+        $('._360vc-cb.play').addClass('videocontrolinactive')
+        $('._360vc-cb.pause').removeClass('videocontrolinactive')
+        $('.room-videos').css('opacity', '0');
+        setTimeout(function(){
+            $('.room-videos').addClass('inactive');
+            $('.room-videos').removeClass('active');
+            $('._360videocontainer').removeClass('modal-active');
+        }, 750)
+    })
+    let isWaiting = false;
+    $(currentPlayer).on('waiting', function(){ isWaiting = true; })
+    $(currentPlayer).on('playing', function(){ isWaiting = false; })
+    $(currentPlayer).parents("._360videocontainer").on('classChange', function () {
+        setTimeout(function(){
+            if(player.paused() && isWaiting === false){
+                player.play();
+            }
+        },50)
+    });
+})
+
+let viewer = pannellum.viewer("panorama", {
+    default: {
+        firstScene: "main",
+        sceneFadeDuration: 1000,
+        //hotSpotDebug: true,
+        mouseZoom: false,
+        keyboardZoom: false
+    },
+    scenes: {
+        main: {
+            type: "equirectangular",
+            panorama:
+                "/spherestory/assets/janssen-01/Spherestory-JanssenGroup-WEBP75.webp",
+            autoLoad: true,
+            showZoomCtrl: false,
+            showFullscreenCtrl: false,
+            pitch: -1.8,
+            yaw: -4.9,
+            hfov: 90,
+            maxHfov: 90,
+            hotSpots: [
+                {
+                    pitch: -4.5,
+                    yaw: -90.3,
+                    type: "info",
+                    cssClass: "custom-hotspot sphere-hotspot_company"
+                },
+                {
+                    pitch: 0.4,
+                    yaw: 81.29,
+                    type: "info",
+                    cssClass: "custom-hotspot sphere-hotspot_video-1",
+                    clickHandlerFunc: hotspotAction,
+                    clickHandlerArgs: {hotspot: "sphere-hotspot_video-1", type: "video-modal"}
+                },
+                {
+                    pitch: -3.51,
+                    yaw: -31.2,
+                    type: "info",
+                    cssClass: "custom-hotspot sphere-hotspot_video-2",
+                    clickHandlerFunc: hotspotAction,
+                    clickHandlerArgs: {hotspot: "sphere-hotspot_video-2", type: "video-modal"}
+                }
+            ]
+        }
+    }
+});
+
+let mm = gsap.matchMedia();
+mm.add("(max-width: 768px)", function() {
+    let animTime = 1750;
+    viewer.on("load", function (){
+        viewer.setHfov(55, animTime);
+        let currentScene  = viewer.getScene()
+        if(currentScene === 'main'){
+            viewer.setPitch(-3.54, animTime)
+            viewer.setYaw(-31, animTime)
+        }
+    })
+});
+viewer.on("load", function () {
+    $("#ch-wrapper_company").clone().appendTo(".sphere-hotspot_company");
+    $("#ch-wrapper_video-1").clone().appendTo(".sphere-hotspot_video-1");
+    $("#ch-wrapper_video-2").clone().appendTo(".sphere-hotspot_video-2");
+    let currentScene = viewer.getScene();
+});
+$('.compass-icon').clone().appendTo('.pnlm-orientation-button');
---
+++ resources/views/spherestory/janssen-01.blade.php
@@ -0,0 +1,166 @@
+
+<!DOCTYPE html><!--  Last Published: Wed Jul 26 2023 14:20:40 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="64c12511563b2b75278a04b1" data-wf-site="64be6439dcf898bf389bbc80">
+<head>
+    <meta charset="utf-8">
+    <title>Flexibility Limburg</title>
+    <meta content="Flexibility Limburg" property="og:title">
+    <meta content="Flexibility Limburg" property="twitter:title">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+
+    <link href="/spherestory/css/flexibility-limburg/normalize.css" rel="stylesheet" type="text/css">
+    <link href="/spherestory/css/flexibility-limburg/components.css" rel="stylesheet" type="text/css">
+    <link href="/spherestory/css/flexibility-limburg/apply-360.css" rel="stylesheet" type="text/css">
+    <link href="/spherestory/css/flexibility-limburg/pagecode.css" rel="stylesheet" type="text/css">
+    <link href="/spherestory/css/janssen/colors.css" rel="stylesheet" type="text/css">
+
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+    <script type="text/javascript">WebFont.load({  google: {    families: ["Inconsolata:400,700"]  }});</script>
+    <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+    <link href="/spherestory/assets/flexibility-limburg/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="/spherestory/assets/flexibility-limburg/webclip.png" rel="apple-touch-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css">
+    <link href="https://cdn.jsdelivr.net/npm/video.js@8.3.0/dist/video-js.min.css" rel="stylesheet">
+</head>
+<body class="_360body">
+<div class="pagewrap">
+    <div class="_360wrapper demo">
+        <div id="panorama" class="_360viewer demo"></div>
+        <div class="room-videos inactive">
+            <div class="_360videocontainer">
+                <div class="_360vc_inner">
+                    <div class="_360videoembed w-embed"><video id="sphere-hotspot_video-1" class="video-js vjs-16-9" playsinline="" preload="auto">
+                            <source src="/spherestory/assets/munckhof-01/Munckhof-01-02.mp4" type="video/mp4">
+                        </video></div>
+                    <div class="_360videocontrols">
+                        <div class="_360vc-cb play videocontrolinactive">
+                            <div class="playiconembed w-embed"><svg class="playbtn-icon" width="16" height="18" viewbox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15.5 8.13398C16.1667 8.51888 16.1667 9.48113 15.5 9.86603L2 17.6603C1.33334 18.0452 0.500001 17.564 0.500001 16.7942L0.500002 1.20577C0.500002 0.435972 1.33334 -0.0451547 2 0.339746L15.5 8.13398Z" fill="white" fill-opacity="0.75"></path>
+                                </svg></div>
+                        </div>
+                    </div>
+                    <div class="_360vc-subcontrols">
+                        <div class="_360vc-cb pause">
+                            <div class="pauzeiconembed w-embed"><svg class="pauzebtn-icon" width="14" height="15" viewbox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="5" height="15" rx="1" fill="#E9E0DE"></rect>
+                                    <rect x="9" width="5" height="15" rx="1" fill="#E9E0DE"></rect>
+                                </svg></div>
+                        </div>
+                        <div class="_360vc_close">
+                            <p class="ch_p16-m">Sluiten</p>
+                        </div>
+                    </div>
+                    <div class="_360vc-cta_wrap">
+                        <a target="_parent" href="https://www.chauffeurbijmunckhof.nl/?id=sollicitatieformulier/#solliciteer" class="_360vc_btn w-inline-block">
+                            <p class="ch_p16-m">Solliciteer direct</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="_360videocontainer">
+                <div class="_360vc_inner">
+                    <div class="_360videoembed w-embed"><video id="sphere-hotspot_video-2" class="video-js vjs-16-9" playsinline="" preload="auto">
+                            <source src="/spherestory/assets/munckhof-01/Munckhof-01-01.mp4" type="video/mp4">
+                        </video></div>
+                    <div class="_360videocontrols">
+                        <div class="_360vc-cb play videocontrolinactive">
+                            <div class="playiconembed w-embed"><svg class="playbtn-icon" width="16" height="18" viewbox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15.5 8.13398C16.1667 8.51888 16.1667 9.48113 15.5 9.86603L2 17.6603C1.33334 18.0452 0.500001 17.564 0.500001 16.7942L0.500002 1.20577C0.500002 0.435972 1.33334 -0.0451547 2 0.339746L15.5 8.13398Z" fill="white" fill-opacity="0.75"></path>
+                                </svg></div>
+                        </div>
+                    </div>
+                    <div class="_360vc-subcontrols">
+                        <div class="_360vc-cb pause">
+                            <div class="pauzeiconembed w-embed"><svg class="pauzebtn-icon" width="14" height="15" viewbox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="5" height="15" rx="1" fill="#E9E0DE"></rect>
+                                    <rect x="9" width="5" height="15" rx="1" fill="#E9E0DE"></rect>
+                                </svg></div>
+                        </div>
+                        <div class="_360vc_close">
+                            <p class="ch_p16-m">Sluiten</p>
+                        </div>
+                    </div>
+                    <div class="_360vc-cta_wrap">
+                        <a target="_parent" href="https://www.chauffeurbijmunckhof.nl/?id=sollicitatieformulier/#solliciteer" class="_360vc_btn w-inline-block">
+                            <p class="ch_p16-m">Solliciteer direct</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="icons">
+            <div class="w-embed"><svg class="compass-icon" width="18" height="18" viewbox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.75693 7.76822C7.25583 8.26363 7.25583 9.07793 7.75693 9.58473C8.25804 10.0801 9.07233 10.0801 9.56774 9.57903C10.0805 9.06628 10.0713 8.26606 9.57344 7.76822C9.07327 7.26802 8.27465 7.2505 7.75693 7.76822Z" fill="currentColor"></path>
+                    <path d="M16.5095 0.0584089C5.72637 5.33137 5.62857 5.35893 5.57056 5.4168C5.48517 5.46807 5.41684 5.54779 5.37127 5.63889C2.10152 12.336 0.78244 15.0378 0.303816 16.0182C0.30434 16.017 0.301936 16.0221 0.296276 16.0336C0.298742 16.0286 0.300587 16.0248 0.301874 16.0222C0.296487 16.0332 0.291032 16.0444 0.285884 16.0549C0.286163 16.0543 0.286369 16.0539 0.286648 16.0534C0.280811 16.0653 0.272986 16.0813 0.263778 16.1002C0.251615 16.1251 0.240113 16.1486 0.229197 16.171C0.23156 16.1661 0.233854 16.1614 0.236183 16.1567C0.226173 16.1772 0.215052 16.2 0.202434 16.2258C0.193465 16.2442 0.184461 16.2626 0.176472 16.279C0.181335 16.2691 0.186136 16.2592 0.190965 16.2493C0.17887 16.274 0.165699 16.3011 0.151309 16.3306C0.118185 16.3984 0.0951452 16.4455 0.080858 16.4748C0.0824582 16.4716 0.0839849 16.4684 0.0857217 16.4649C0.080927 16.4747 0.0762686 16.4842 0.0712633 16.4945C0.0706369 16.4958 0.0697712 16.4976 0.069179 16.4988C0.0693897 16.4983 0.0697028 16.4977 0.0699078 16.4973C0.0659104 16.5054 0.0625058 16.5124 0.0584058 16.5208C-0.0497873 16.7429 -0.0042274 17.0049 0.166598 17.1757C0.280491 17.2839 0.422851 17.3408 0.57091 17.3408C0.656303 17.3408 0.741736 17.3237 0.821463 17.2839C0.821429 17.2839 0.821497 17.2838 0.821463 17.2839C0.820421 17.2844 0.821634 17.2838 0.824066 17.2826C0.823439 17.2829 0.82185 17.2837 0.821463 17.2839C0.822955 17.2831 0.824515 17.2824 0.825973 17.2817C0.828894 17.2802 0.833552 17.278 0.839873 17.2749C0.839281 17.2752 0.8389 17.2753 0.838342 17.2756C0.839526 17.275 0.840534 17.2745 0.841713 17.274C0.844389 17.2727 0.846507 17.2716 0.849741 17.27C0.848141 17.2708 0.846303 17.2717 0.844805 17.2724C0.848489 17.2706 0.851758 17.269 0.855408 17.2673C0.877024 17.2567 0.909697 17.2407 0.956306 17.2179C0.954427 17.2188 0.952554 17.2197 0.950709 17.2206C1.01299 17.1902 1.06481 17.1648 1.10746 17.144C1.8733 16.7693 4.43017 15.5183 11.692 11.9653C11.8798 11.8717 11.922 11.7445 11.9654 11.6749L17.2725 0.821415C17.3807 0.605029 17.3352 0.337405 17.1643 0.166585C16.9878 -0.0042235 16.7259 -0.0497899 16.5095 0.0584089ZM1.19961 17.0989C1.2125 17.0926 1.2237 17.0871 1.23315 17.0824C1.22328 17.0873 1.21188 17.0929 1.19961 17.0989ZM10.3766 10.388C9.42921 11.324 7.91171 11.3338 6.95429 10.388C6.00904 9.43703 6.00904 7.89954 6.94862 6.95999C7.8953 6.01331 9.4282 6.00539 10.3766 6.96569C11.3066 7.884 11.3436 9.40898 10.3766 10.388Z" fill="currentColor"></path>
+                </svg></div>
+        </div>
+    </div>
+</div>
+<div class="hotspot-wrapper demo">
+    <div id="ch-wrapper_company" class="ch-wrapper">
+        <div class="ch-inner">
+            <div class="ch-avatar-wrap"><img src="/spherestory/assets/janssen-01/Janssen-Logo.jpg" loading="lazy" alt="" class="avatar"></div>
+            <div class="ch-details">
+                <div class="ch-details-title">
+                    <p class="ch_p20-r">Munckhof</p>
+                </div>
+                <div class="ch-details-desc">
+                    <p class="ch_p14-m">Wordt dit jouw nieuwe werkplek?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="ch-wrapper_video-1" class="ch-wrapper clickable">
+        <div class="ch-inner">
+            <div class="ch-avatar-wrap"><img src="/spherestory/assets/janssen-01/Janssen-Logo.jpg" loading="lazy" alt="" class="avatar">
+                <div class="ch-avatar-actionicon">
+                    <div class="html-embed-2 w-embed"><svg class="action-icon-play" style="width:.5em;height:auto;" width="8" height="9" viewbox="0 0 8 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.82564 8.39667L7.53246 5.03087C7.96372 4.79114 7.95369 4.20188 7.53246 3.96218L1.82564 0.596361C1.39439 0.336691 0.822754 0.53644 0.822754 1.02584V7.96722C0.822754 8.44657 1.35434 8.67628 1.82564 8.39667Z" fill="white" fill-opacity="0.85"></path>
+                        </svg></div>
+                </div>
+            </div>
+            <div class="ch-details">
+                <div class="ch-details-title">
+                    <p class="ch_p20-r">Rick de Oude</p>
+                </div>
+                <div class="ch-details-desc">
+                    <p class="ch_p14-m">Bedrijfsleider</p>
+                </div>
+            </div>
+        </div>
+        <div class="ch-clickable_tint animate"></div>
+        <div class="ch-clickable_tint hover"></div>
+    </div>
+    <div id="ch-wrapper_video-2" class="ch-wrapper clickable">
+        <div class="ch-inner">
+            <div class="ch-avatar-wrap"><img src="/spherestory/assets/janssen-01/Janssen-Logo.jpg" loading="lazy" alt="" class="avatar">
+                <div class="ch-avatar-actionicon">
+                    <div class="html-embed-2 w-embed"><svg class="action-icon-play" style="width:.5em;height:auto;" width="8" height="9" viewbox="0 0 8 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.82564 8.39667L7.53246 5.03087C7.96372 4.79114 7.95369 4.20188 7.53246 3.96218L1.82564 0.596361C1.39439 0.336691 0.822754 0.53644 0.822754 1.02584V7.96722C0.822754 8.44657 1.35434 8.67628 1.82564 8.39667Z" fill="white" fill-opacity="0.85"></path>
+                        </svg></div>
+                </div>
+            </div>
+            <div class="ch-details">
+                <div class="ch-details-title">
+                    <p class="ch_p20-r">Cyril Hunte</p>
+                </div>
+                <div class="ch-details-desc">
+                    <p class="ch_p14-m">Chauffeur</p>
+                </div>
+            </div>
+        </div>
+        <div class="ch-clickable_tint hover"></div>
+        <div class="ch-clickable_tint animate"></div>
+    </div>
+</div>
+<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=64be6439dcf898bf389bbc80" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="/spherestory/js/flexibility-limburg/apply-360.js" type="text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/video.js@8.3.0/dist/video.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" integrity="sha512-16esztaSRplJROstbIIdwX3N97V1+pZvV33ABoG1H2OyTttBxEGkTsoIVsiP1iaTtM8b3+hu2kB6pQ4Clr5yug==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/splidejs/4.1.4/js/splide.min.js" integrity="sha512-4TcjHXQMLM7Y6eqfiasrsnRCc8D/unDeY1UGKGgfwyLUCTsHYMxF7/UHayjItKQKIoP6TTQ6AMamb9w2GMAvNg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/spherestory/js/munckhof-01/pagescript.js" type="text/javascript"></script>
+</body>
+</html>
