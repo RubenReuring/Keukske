32f2c4c2b6833d5d671d6052720727dfc01443f6
rubenmachiels <ruben@reuring.studio>
2024-02-20 16:58:09 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/script.js
+++ public/site-snippets/erik-aarts/script.js
@@ -11,18 +11,18 @@ $(document).ready(function() {
     navbarTransition.to(
         navbarWordWrapArray,
         {width: "0", duration: .8, ease: "power4.inOut"},
-        .75
+        .65
     );
     navbarTransition.to(
         '.home-link__name',
         {gridColumnGap: "0.075em", duration: .9, ease: "power4.inOut"},
-        .75
+        .65
     );
     navbarTransition.fromTo(
         '.navbar',
         {backgroundColor: 'rgba(255,255,255,0)'},
         {paddingTop: "1em", paddingBottom: "1em", backgroundColor: 'rgba(255,255,255,1)',duration: .8, ease: "power4.out"},
-        .75
+        .65
     );
 
 
