a72c5d0f3c319b3fc9b52985c842851458b93a58
rubenmachiels <ruben@reuring.studio>
2024-02-20 12:18:33 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/script.js
+++ public/site-snippets/erik-aarts/script.js
@@ -1,1 +1,6 @@
-console.log('hello')
+let navbarTransition = gsap.timeline();
+navbarTransition.fromTo(
+    '.home-link__name',
+    {y: "125%"},
+    {y: "0%", stagger: 0.1, duration: 1, ease: "power3.out"}
+);
