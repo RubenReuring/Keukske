e2a4e7013ac58eaa5211cc7c91cd074b7adc7923
rubenmachiels <ruben@reuring.studio>
2024-02-20 18:38:38 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/script.js
+++ public/site-snippets/erik-aarts/script.js
@@ -46,6 +46,23 @@ $(document).ready(function() {
             navbarTransition.play();
         }
     });
+
+
+    let reviewsSplide = new Splide(".hri-splide", {
+        type: "fade",
+        rewind: true,
+        perPage: 1,
+        perMove: 1,
+        pagination: false,
+        rewind: true,
+        classes: {
+            arrows: "splide__arrows hri-controls",
+            arrow: "splide__arrow hri-arrow_button",
+            prev: "splide__arrow--prev hri-arrow_button.prev",
+            next: "splide__arrow--next hri-arrow_button.next",
+        },
+    }).mount();
+
 });
 
 
