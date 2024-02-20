6dc8a10263f91710b4b3948854d96b0450ad6f95
rubenmachiels <ruben@reuring.studio>
2024-02-20 12:23:36 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/script.js
+++ public/site-snippets/erik-aarts/script.js
@@ -4,3 +4,8 @@ navbarTransition.to(
     navbarWordArray,
     {y: "100%", stagger: 0.1, duration: 1, ease: "power3.out"}
 );
+navbarTransition.fromTo(
+    navbarWordArray,
+    {width: "0%"},
+    {width: "100%", stagger: 0.1, duration: 1, ease: "power3.out"}
+);
