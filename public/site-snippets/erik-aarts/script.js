e86d259ef9d9be8a00fafc38c9f0c6693ded6792
rubenmachiels <ruben@reuring.studio>
2024-02-20 12:31:17 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/script.js
+++ public/site-snippets/erik-aarts/script.js
@@ -3,11 +3,11 @@ let navbarWordArray = $('.hln-word__secondary');
 let navbarWordWrapArray = $('.hln-word__secondary--wrap');
 navbarTransition.to(
     navbarWordArray,
-    {y: "100%", stagger: 0.1, duration: .65, ease: "power3.out"}
+    {y: "100%", stagger: 0.15, duration: .95, ease: "power3.out"}
 );
 navbarTransition.to(
     navbarWordWrapArray,
-    {width: "0", stagger: 0.1, duration: .9, ease: "power3.out"},
-    .3
+    {width: "0", stagger: 0.15, duration: 1.1, ease: "power3.out"},
+    .65
 );
 
