2d061c2afb4b92b1661ed4a927f4ccb2cb21ce06
rubenmachiels <ruben@reuring.studio>
2024-02-21 13:58:12 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/buitenplans/script.js
+++ public/site-snippets/buitenplans/script.js
@@ -1,5 +1,5 @@
 let loader = gsap.timeline();
 loader.to(
     '.loader-inner',
-    {y: "48vh", duration: .8, ease: "power4.inOut"},
+    {y: "48vh", maskPosition: '0px -48vh', duration: .8, ease: "power4.inOut"},
 );
