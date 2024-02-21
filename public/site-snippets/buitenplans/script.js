db7ec22255e3cfc29b6b653e459532ae8086179a
rubenmachiels <ruben@reuring.studio>
2024-02-21 14:53:30 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/buitenplans/script.js
+++ public/site-snippets/buitenplans/script.js
@@ -2,35 +2,40 @@ let loader = gsap.timeline();
 loader.fromTo(
     '.loader__inner',
     {opacity: '0',},
-    {opacity: "1", duration: .3, ease: "power2.inOut"},
+    {opacity: "1", duration: .4, ease: "power2.inOut"},
 );
 loader.fromTo(
     '.ll-center__mask',
     {y: '100%',},
     {y: "0%", duration: 2, ease: "power4.inOut"},
-    .3
+    .6
 );
 loader.fromTo(
     '.loader-logo__embed.fill',
     {y: "-100%",},
     {y: "0%", duration: 2, ease: "power4.inOut"},
-    0.3
+    .6
 );
 loader.fromTo(
     '.lcw-title__inner',
     {y: '100%',},
     {y: "0%", duration: .7, ease: "power4.inOut"},
-    .9
+    1
 );
 loader.fromTo(
     '.lcw-tag__inner',
     {y: '100%',},
     {y: "0%", duration: .7, ease: "power4.inOut"},
-    1
+    1.2
 );
 loader.fromTo(
     '.loader',
     {y: '0%',},
     {y: "-100%", duration: 1, ease: "power4.inOut"},
-    1.75
+    1.85
+);
+loader.fromTo(
+    '.loader__inner',
+    {opacity: '1',},
+    {opacity: "0", duration: .4, ease: "power2.inOut"},
 );
