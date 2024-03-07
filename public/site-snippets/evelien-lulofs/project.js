c72f4ed7c5be5c61fe9505b8a40326a6f0936776
rubenmachiels <ruben@reuring.studio>
2024-03-07 17:01:29 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/evelien-lulofs/project.js
+++ public/site-snippets/evelien-lulofs/project.js
@@ -42,6 +42,6 @@ $('.pdi-lg__rte').children('p').each(function(){
         console.log('empty');
     } else {
         // If <p> has text
-        console.log('Inner:' + $(this).text());
+        console.log(ParagraphText.length);
     }
 })
