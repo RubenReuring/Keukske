073cf2bd13456c1cfcbe0a685f1830c114d1e036
rubenmachiels <ruben@reuring.studio>
2024-03-08 13:34:26 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/evelien-lulofs/main.js
+++ public/site-snippets/evelien-lulofs/main.js
@@ -2,9 +2,9 @@ console.log($('.home-visual, .home-werkwijze'))
 let lightNavElems = $('.home-visual, .home-werkwijze')
 
 
-
+let st
     $(lightNavElems).each(function(){
-        let st = ScrollTrigger.create({
+        st = ScrollTrigger.create({
             trigger: this,
             start: 'top bottom',
             end: 'bottom bottom',
