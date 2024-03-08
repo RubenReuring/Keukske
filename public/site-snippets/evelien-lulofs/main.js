e570259743d1f9a566b055fd9d8749cf48fe7fe7
rubenmachiels <ruben@reuring.studio>
2024-03-08 13:11:49 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/evelien-lulofs/main.js
+++ public/site-snippets/evelien-lulofs/main.js
@@ -1,1 +1,11 @@
 console.log($('.home-visual, .home-werkwijze'))
+let lightNavElems = $('.home-visual, .home-werkwijze')
+
+$(lightNavElems).each(function(){
+    ScrollTrigger.create({
+        trigger: this,
+        start: 'top top',
+        end: 'bottom bottom',
+        markers : true
+    });
+});
