92fba172563eb81887baa90f94f02d73fe27c880
rubenmachiels <ruben@reuring.studio>
2024-03-11 12:57:53 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/evelien-lulofs/main.js
+++ public/site-snippets/evelien-lulofs/main.js
@@ -11,7 +11,7 @@ function toggleNavLight(turnOn) {
     $(lightNavElems).each(function(){
         st = ScrollTrigger.create({
             trigger: this,
-            start: 'top' + ($('.navbar').outerHeight() - ( $('.navbar').height() / 2 )),
+            start: 'top' + 10,
             end: 'bottom top',
             markers : true,
             onEnter: function(){
