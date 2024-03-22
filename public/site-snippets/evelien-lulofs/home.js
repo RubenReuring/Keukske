12237fc8f5a780de436c59d966c2dea396512d89
rubenmachiels <ruben@reuring.studio>
2024-03-22 10:52:37 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/evelien-lulofs/home.js
+++ public/site-snippets/evelien-lulofs/home.js
@@ -187,7 +187,7 @@ $(document).ready(function () {
         2.6
     );
     heroTextLoad.fromTo(
-        ".hhm-bottom",
+        ".hmm-top__label-tag",
         {y: "25%", opacity: 0},
         {y: "0%", opacity: 1, duration: 0.75, ease: "power1.out"},
         2.9
@@ -223,66 +223,6 @@ $(document).ready(function () {
     );
     heroTextLoad.eventCallback("onComplete", function(){  });
 
-    /// GSAP Link Hover
-    //let text = new SplitText(".link-paragraph", {type: "chars"});
-
-    // $(".link12-12-sb-test").each(function () {
-    //     let innerText = $(this).find(".link-paragraph").children();
-    //     $(this).find(".link-paragraph").children().each(function () {
-    //             let innerLetter = $(this).text();
-    //             $(this).wrapInner("<span class='link-paragraph__inner--visible' style='display: inline-block; position: relative;'></span>");
-    //             $('<span class="link-paragraph__inner--hidden" style="display: inline-block; position: absolute; left: -100%;">' + innerLetter + "</span>").appendTo($(this));
-    //         });
-    //
-    //     let hoverText = $(this).find('.link-paragraph').children();
-    //     $(hoverText).each(function () {
-    //         let linkAnimation = gsap.to(this.children, {
-    //             x: "+=100%",
-    //             paused: true,
-    //             ease: "power1.inOut",
-    //             //duration: 0.2,
-    //             stagger: {
-    //                 amount: .065,
-    //             },
-    //         });
-    //
-    //         $(this)
-    //             .parents(".link12-12-sb-test")
-    //             .on("mouseenter", function () {
-    //                 linkAnimation.play();
-    //             });
-    //         $(this)
-    //             .parents(".link12-12-sb-test")
-    //             .on("mouseleave", function () {
-    //                 linkAnimation.reverse();
-    //             });
-    //     });
-    // });
-
-    // $(".link12-12-sb").each(function () {
-    //     let hoverText = $(this).children();
-    //     $(hoverText).each(function () {
-    //         let linkAnimation = gsap.to(this.children, {
-    //             y: "-=100%",
-    //             paused: true,
-    //             ease: "power1.inOut",
-    //             duration: 0.35,
-    //             stagger: {
-    //                 amount: 0.175,
-    //             },
-    //         });
-    //         $(this)
-    //             .parents(".link12-12-sb")
-    //             .on("mouseenter", function () {
-    //                 linkAnimation.play();
-    //             });
-    //         $(this)
-    //             .parents(".link12-12-sb")
-    //             .on("mouseleave", function () {
-    //                 linkAnimation.reverse();
-    //             });
-    //     });
-    // });
     $(".link12-12-sb").each(function () {
             let linkAnimation = gsap.to(this.children, {
                 y: "-=100%",
