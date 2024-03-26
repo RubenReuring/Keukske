31d2ec5a1f94e87308e2a4f60dcf4f61593b3a99
rubenmachiels <ruben@reuring.studio>
2024-03-26 22:34:58 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/project.js
+++ public/site-snippets/erik-aarts/project.js
@@ -1,14 +1,45 @@
-let blurStickySection = ScrollTrigger.create({
-    trigger: $('.pc-image__collection'),
-    start: () => 'top bottom',
-    end: () => 'bottom top',
-    markers : true,
-    invalidateOnRefresh: true,
-    onEnter: function(){
-        console.log('a')
-        $('.ps-image__blur').addClass('visible')
-    },
-    onLeaveBack: function(){
-        $('.ps-image__blur').removeClass('visible')
+
+
+$(document).ready(function() {
+
+    let blurStickySection = ScrollTrigger.create({
+        trigger: $('.pc-image__collection'),
+        start: () => 'top bottom',
+        end: () => 'bottom top',
+        markers: true,
+        invalidateOnRefresh: true,
+        onEnter: function () {
+            $('.ps-image__blur').addClass('visible')
+        },
+        onLeaveBack: function () {
+            $('.ps-image__blur').removeClass('visible')
+        }
+    });
+
+    let navbarTransition = gsap.timeline({paused: true});
+    navbarTransition.fromTo(
+        '.navbar',
+        {backgroundColor: 'rgba(255,255,255,0)'},
+        {paddingTop: "1em", paddingBottom: "1em", backgroundColor: 'rgba(255,255,255,1)',duration: .8, ease: "power4.out"},
+        .1
+    );
+
+    function collapsNav() {
+        navbarTransition.play();
     }
+
+    if (isAtTop()) {
+        navbarTransition.reverse();
+    } else {
+        navbarTransition.play();
+    }
+
+    $(window).scroll(function() {
+        if (isAtTop()) {
+            navbarTransition.reverse();
+        } else {
+            navbarTransition.play();
+        }
+    });
+
 });
