b1881001ee864fed0406655450513ce012f6729c
rubenmachiels <ruben@reuring.studio>
2024-03-27 11:06:10 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/main.js
+++ public/site-snippets/erik-aarts/main.js
@@ -2,7 +2,6 @@ function isAtTop() {
     return $(window).scrollTop() === 0;
 }
 
-
 // GSAP Accordion for each Step Item
 $(".ab-item").each(function (i) {
     const accordionTimeline = gsap
@@ -18,3 +17,19 @@ $(".ab-item").each(function (i) {
 function toggleAccordion(timeline) {
     timeline.reversed() ? timeline.play() : timeline.reverse();
 }
+
+
+
+    const voorwaardenTimeline = gsap.timeline({reversed: true, paused: true})
+        .to($('.voorwaarden').find(".vi-content__wrap"), {
+            height: "auto",
+            duration: 1,
+            ease: "power3.inOut",
+        });
+    $('.voorwaarden').find(".link16-16-med").on("click", () => toggleVoorwaarden(voorwaardenTimeline));
+
+function toggleVoorwaarden(timeline) {
+    timeline.reversed() ? timeline.play() : timeline.reverse();
+}
+
+
