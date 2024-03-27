4089b940e33e073ad84747382b4b2d76047acce3
rubenmachiels <ruben@reuring.studio>
2024-03-27 15:27:09 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/main.js
+++ public/site-snippets/erik-aarts/main.js
@@ -21,7 +21,7 @@ function toggleAccordion(timeline) {
     const voorwaardenTimeline = gsap.timeline({reversed: true, paused: true}).to($('.voorwaarden').find(".vi-content__wrap"), {height: "auto", duration: 1, ease: "power3.inOut",});
 
     $('.voorwaarden').find(".link16-16-med").on("click", () => {
-        if($('.voorwaarden').find(".link16-16-med").hasClass('enabled')){ toggleVoorwaarden(voorwaardenTimeline);}
+        if(!$('.voorwaarden').find(".link16-16-med").hasClass('disabled')){ toggleVoorwaarden(voorwaardenTimeline);}
     });
     $('.voorwaarden').find(".link12-12-med").on("click", () => {
         if($('.voorwaarden').find(".link12-12-med").hasClass('enabled')){ toggleVoorwaarden(voorwaardenTimeline);}
@@ -29,7 +29,7 @@ function toggleAccordion(timeline) {
 
 function toggleVoorwaarden(timeline) {
     timeline.reversed() ? timeline.play() : timeline.reverse();
-    timeline.reversed() ? $('.voorwaarden').find(".link16-16-med").addClass('enabled') : $('.voorwaarden').find(".link16-16-med").removeClass('enabled');
+    timeline.reversed() ? $('.voorwaarden').find(".link16-16-med").removeClass('disabled') : $('.voorwaarden').find(".link16-16-med").addClass('disabled');
     timeline.reversed() ? $('.voorwaarden').find(".link12-12-med").removeClass('enabled') : $('.voorwaarden').find(".link12-12-med").addClass('enabled');
     $('.voorwaarden').hasClass('open') ? $('.voorwaarden').removeClass('open') : $('.voorwaarden').addClass('open');
 }
