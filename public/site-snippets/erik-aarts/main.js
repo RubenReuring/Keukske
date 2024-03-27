16cefc6097de63a99873ad9fd4d0d980738291b4
rubenmachiels <ruben@reuring.studio>
2024-03-27 15:25:49 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/main.js
+++ public/site-snippets/erik-aarts/main.js
@@ -30,6 +30,7 @@ function toggleAccordion(timeline) {
 function toggleVoorwaarden(timeline) {
     timeline.reversed() ? timeline.play() : timeline.reverse();
     timeline.reversed() ? $('.voorwaarden').find(".link16-16-med").addClass('enabled') : $('.voorwaarden').find(".link16-16-med").removeClass('enabled');
+    timeline.reversed() ? $('.voorwaarden').find(".link12-12-med").removeClass('enabled') : $('.voorwaarden').find(".link12-12-med").addClass('enabled');
     $('.voorwaarden').hasClass('open') ? $('.voorwaarden').removeClass('open') : $('.voorwaarden').addClass('open');
 }
 
