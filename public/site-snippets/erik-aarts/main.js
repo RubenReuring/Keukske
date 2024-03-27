d9fdb5239fb91d5b210ab543b1812573c95a91f7
rubenmachiels <ruben@reuring.studio>
2024-03-27 10:47:16 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/erik-aarts/main.js
+++ public/site-snippets/erik-aarts/main.js
@@ -12,7 +12,7 @@ $(".ab-item").each(function (i) {
             duration: 1,
             ease: "power3.inOut",
         });
-    $(this).find(".ab-accordion__head).on("click", () => toggleAccordion(accordionTimeline));
+    $(this).find(".ab-accordion__head").on("click", () => toggleAccordion(accordionTimeline));
 });
 
 function toggleAccordion(timeline) {
