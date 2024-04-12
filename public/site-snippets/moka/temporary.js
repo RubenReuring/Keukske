42837334a2852aecd24528e2db8af4159caac9b0
rubenmachiels <ruben@reuring.studio>
2024-04-12 13:54:00 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/moka/temporary.js
+++ public/site-snippets/moka/temporary.js
@@ -1,1 +1,12 @@
-console.log('moka')
+
+$(document).ready(function(){
+
+    console.log('moka')
+    let loader = gsap.timeline();
+    loader.to(
+        '.hvl-inner',
+        {width: "100%", duration: .75, ease: "power3.inOut"}
+    );
+
+
+});
