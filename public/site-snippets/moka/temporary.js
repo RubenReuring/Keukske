eef503bf2c84c9ba877e7f232892020d4bc04880
rubenmachiels <ruben@reuring.studio>
2024-04-14 14:34:02 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/moka/temporary.js
+++ public/site-snippets/moka/temporary.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
 
     console.log('moka')
     let loader = gsap.timeline();
+    loader.fromTo(
+        '.hvl-inner',
+        {opaicty: "0"},
+        {opacity: "1", duration: .45, ease: "power1.inOut"}
+    );
     loader.to(
         '.hvl-line',
         {width: "0em", duration: .35, ease: "power1.inOut"}
