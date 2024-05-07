d8d2fe65282db5ff4101470e18482ab777f66848
rubenmachiels <ruben@reuring.studio>
2024-05-07 20:05:48 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/reuring/slider-experimental.js
+++ public/site-snippets/reuring/slider-experimental.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     $('.mc-splide__item').on("mouseenter mouseleave", function(event){
         if (event.type === "mouseenter") {
             // Your code here when mouse enters the element
-            $(this).css("background-color", "red");
+            $(this).css("opacity", "1");
         } else if (event.type === "mouseleave") {
             // Your code here when mouse leaves the element
-            $(this).css("background-color", "blue");
+            $(this).css("opacity", ".1");
         }
     });
 })
