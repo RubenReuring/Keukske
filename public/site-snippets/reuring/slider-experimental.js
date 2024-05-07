f8ac0ad6cf48b8ab6a0e8d23ebec14d7075447a5
rubenmachiels <ruben@reuring.studio>
2024-05-07 21:07:14 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/reuring/slider-experimental.js
+++ public/site-snippets/reuring/slider-experimental.js
@@ -13,16 +13,17 @@ $(document).ready(function(){
     });
 
     $('.mc-splide__item').on('click', function(){
-        if($(this).hasClass('item-active')){
-            $(this).removeClass('item-active')
-            $(this).prev().removeClass("sibling-active")
-            $(this).next().removeClass("sibling-active")
-        }
 
         if(!$(this).hasClass('item-active')){
             $(this).addClass('item-active')
             $(this).prev().addClass("sibling-active")
             $(this).next().addClass("sibling-active")
+        }
+
+        if($(this).hasClass('item-active')){
+            $(this).removeClass('item-active')
+            $(this).prev().removeClass("sibling-active")
+            $(this).next().removeClass("sibling-active")
         }
 
     });
