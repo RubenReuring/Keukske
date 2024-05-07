6ec4f742c228e6fe0bdc6d5453dc35f5955d664e
rubenmachiels <ruben@reuring.studio>
2024-05-07 21:38:45 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/reuring/slider-experimental.js
+++ public/site-snippets/reuring/slider-experimental.js
@@ -21,8 +21,15 @@ $(document).ready(function(){
             $(this).next().addClass("sibling-active")
 
             let imageURL = $(this).find('img').attr('src');
+
+            $('.main-content__imagewrap.overlay').find('img').on('load', function(){
+                // Image has finished loading, now you can execute an alert
+                alert('Image loaded successfully!');
+            });
+
             $('.main-content__imagewrap.overlay').find('img').attr('src', imageURL)
-            $('.main-content__imagewrap.overlay').css('height', '65vh')
+
+            //$('.main-content__imagewrap.overlay').css('height', '65vh')
             //Set dom item
             //Set image to dom item
             //Scale animation
