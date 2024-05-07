84930c4f8c8460fa7858b20b0bf6e296bf68af19
rubenmachiels <ruben@reuring.studio>
2024-05-07 20:58:16 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/reuring/slider-experimental.js
+++ public/site-snippets/reuring/slider-experimental.js
@@ -2,31 +2,19 @@ $(document).ready(function(){
 
     $('.mc-splide__item').on("mouseenter mouseleave", function(event){
         if (event.type === "mouseenter") {
-            $(this).css("opacity", "1")
-            $(this).prev().css("opacity", ".5");
-            $(this).next().css("opacity", ".5");
-
-            $(this).css("height", "6em")
-            $(this).prev().css("height", "5.5em")
-            $(this).next().css("height", "5.5em");
+            $(this).addClass("item-hover")
+            $(this).prev().addClass("sibling-hover")
+            $(this).next().addClass("sibling-hover")
         } else if (event.type === "mouseleave") {
-            $(this).css("opacity", ".1")
-            $(this).prev().css("opacity", ".1")
-            $(this).next().css("opacity", ".1");
-
-            $(this).css("height", "5em")
-            $(this).prev().css("height", "5em")
-            $(this).next().css("height", "5em");
+            $(this).removeClass("item-hover")
+            $(this).prev().removeClass("sibling-hover")
+            $(this).next().removeClass("sibling-hover")
         }
     });
 
     $('.mc-splide__item').on('click', function(){
         if(!$(this).hasClass('item-active')){
             $(this).addClass('item-active')
-            $(this).css("height", "6em")
-            $(this).prev().css("height", "5.5em")
-            $(this).next().css("height", "5.5em");
-
             $(this).siblings().removeClass('item-active')
         } else if($(this).hasClass('item-active')){
             //$(this).removeClass('item-active')
