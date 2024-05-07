d2cc058a9f1b440ea7e4d0b3362ed2bda685859b
rubenmachiels <ruben@reuring.studio>
2024-05-07 22:13:38 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/reuring/slider-experimental.js
+++ public/site-snippets/reuring/slider-experimental.js
@@ -11,33 +11,39 @@ $(document).ready(function(){
         }
     });
     $('.mc-splide__item').on('click', function(){
-        if($('.main-content__imagewrap.overlay:last').hasClass('transitioning')){
+        if($(this).hasClass('item-active')){
             return
-        } else if (!$('.main-content__imagewrap.overlay:last').hasClass('transitioning')){
-            $('.mc-splide__item').removeClass('item-active')
-            $('.mc-splide__item').removeClass('sibling-active')
-            if(!$(this).hasClass('item-active')){
-                $(this).addClass('item-active')
-                $(this).prev().addClass("sibling-active")
-                $(this).next().addClass("sibling-active")
+        } else if (!$(this).hasClass('item-active')){
 
-                let imageURL = $(this).find('img').attr('src');
-                $('.main-content').append('<div class="main-content__imagewrap overlay"><img src="" loading="lazy" alt="" class="mci-image"></div>');
-                $('.main-content__imagewrap.overlay:last').find('img').on('load', function(){
-                    $('.main-content__imagewrap.overlay:last').css('height', '65vh')
-                    $('.main-content__imagewrap.overlay:last').addClass('transitioning')
-                });
-                $('.main-content__imagewrap.overlay:last').find('img').attr('src', imageURL)
-                if($('.main-content').find('.main-content__imagewrap.overlay').length > 2){
+            if($('.main-content__imagewrap.overlay:last').hasClass('transitioning')){
+                return
+            } else if (!$('.main-content__imagewrap.overlay:last').hasClass('transitioning')){
+                $('.mc-splide__item').removeClass('item-active')
+                $('.mc-splide__item').removeClass('sibling-active')
+                if(!$(this).hasClass('item-active')){
+                    $(this).addClass('item-active')
+                    $(this).prev().addClass("sibling-active")
+                    $(this).next().addClass("sibling-active")
+
+                    let imageURL = $(this).find('img').attr('src');
+                    $('.main-content').append('<div class="main-content__imagewrap overlay"><img src="" loading="lazy" alt="" class="mci-image"></div>');
+                    $('.main-content__imagewrap.overlay:last').find('img').on('load', function(){
+                        $('.main-content__imagewrap.overlay:last').css('height', '65vh')
+                        $('.main-content__imagewrap.overlay:last').addClass('transitioning')
+                    });
+                    $('.main-content__imagewrap.overlay:last').find('img').attr('src', imageURL)
+                    if($('.main-content').find('.main-content__imagewrap.overlay').length > 2){
+                        $('.main-content__imagewrap.overlay:last').on('transitionend', function(event){
+                            $('.main-content__imagewrap.overlay:last').prev().prevAll('.main-content__imagewrap.overlay').remove();
+
+                        });
+                    }
                     $('.main-content__imagewrap.overlay:last').on('transitionend', function(event){
-                        $('.main-content__imagewrap.overlay:last').prev().prevAll('.main-content__imagewrap.overlay').remove();
-
+                        $(this).removeClass('transitioning')
                     });
                 }
-                $('.main-content__imagewrap.overlay:last').on('transitionend', function(event){
-                    $(this).removeClass('transitioning')
-                });
             }
+
         }
     });
 })
