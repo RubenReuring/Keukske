ce7fa277d151bf4fb70b48dcc09fa8510d6064d8
rubenmachiels <ruben@reuring.studio>
2024-06-14 16:34:35 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/general.js
+++ public/site-snippets/studied/general/general.js
@@ -1,22 +1,28 @@
-$(document).ready(function() {
-    let lastScrollTop = 0;
+function isAtTop() {
+    return $(window).scrollTop() === 0;
+}
 
-    let navbarTransition = gsap.timeline({paused: true});
-    navbarTransition.fromTo(
-        '.nav',
-        {y: '0%'},
-        {y: "-100%", duration: .8, ease: "power2.inOut"},
-    );
+let navbarTransition = gsap.timeline({paused: true});
+navbarTransition.fromTo(
+    '.nav',
+    {backgroundColor: 'rgba(250,248,245,0)'},
+    {paddingTop: "1em", paddingBottom: "1em", backgroundColor: 'rgba(250,248,245,1)',duration: .55, ease: "power2.inOut"},
+);
 
-    $(window).scroll(function() {
-        let currentScrollTop = $(this).scrollTop();
-        if (currentScrollTop > lastScrollTop) {
-            // Scrolling down
-            navbarTransition.play();
-        } else {
-            // Scrolling up
-            navbarTransition.reverse();
-        }
-        lastScrollTop = currentScrollTop;
-    });
+function collapsNav() {
+    navbarTransition.play();
+}
+
+if (isAtTop()) {
+    navbarTransition.reverse();
+} else {
+    navbarTransition.play();
+}
+
+$(window).scroll(function() {
+    if (isAtTop()) {
+        navbarTransition.reverse();
+    } else {
+        navbarTransition.play();
+    }
 });
