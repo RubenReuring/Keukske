270c0e817d9b83d8142c9d4d026a53c4453aeac3
rubenmachiels <ruben@reuring.studio>
2024-06-14 16:32:54 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/general.js
+++ public/site-snippets/studied/general/general.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     let navbarTransition = gsap.timeline({paused: true});
     navbarTransition.fromTo(
         '.nav',
-        {backgroundColor: 'rgba(250,248,245,0)'},
-        {paddingTop: ".75em", paddingBottom: ".75em", backgroundColor: 'rgba(250,248,245,1)', duration: .35, ease: "power2.out"},
+        {y: '0%'},
+        {y: "-100%", duration: .34, ease: "power3.out"},
     );
 
     $(window).scroll(function() {
