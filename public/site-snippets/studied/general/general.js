9ffda24e391669d5cde063bf8895d824d62e6bbc
rubenmachiels <ruben@reuring.studio>
2024-06-14 16:31:51 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/general.js
+++ public/site-snippets/studied/general/general.js
@@ -1,28 +1,22 @@
-function isAtTop() {
-    return $(window).scrollTop() === 0;
-}
+$(document).ready(function() {
+    let lastScrollTop = 0;
 
-let navbarTransition = gsap.timeline({paused: true});
-navbarTransition.fromTo(
-    '.nav',
-    {backgroundColor: 'rgba(250,248,245,0)'},
-    {paddingTop: "0.75em", paddingBottom: "0.75em", backgroundColor: 'rgba(250,248,245,1)',duration: .35, ease: "power2.out"},
-);
+    let navbarTransition = gsap.timeline({paused: true});
+    navbarTransition.fromTo(
+        '.nav',
+        {backgroundColor: 'rgba(250,248,245,0)'},
+        {paddingTop: ".75em", paddingBottom: ".75em", backgroundColor: 'rgba(250,248,245,1)', duration: .35, ease: "power2.out"},
+    );
 
-function collapsNav() {
-    navbarTransition.play();
-}
-
-if (isAtTop()) {
-    navbarTransition.reverse();
-} else {
-    navbarTransition.play();
-}
-
-$(window).scroll(function() {
-    if (isAtTop()) {
-        navbarTransition.reverse();
-    } else {
-        navbarTransition.play();
-    }
+    $(window).scroll(function() {
+        let currentScrollTop = $(this).scrollTop();
+        if (currentScrollTop > lastScrollTop) {
+            // Scrolling down
+            navbarTransition.play();
+        } else {
+            // Scrolling up
+            navbarTransition.reverse();
+        }
+        lastScrollTop = currentScrollTop;
+    });
 });
