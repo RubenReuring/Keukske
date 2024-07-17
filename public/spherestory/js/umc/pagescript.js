01f53684de8c9520e861e2076d2b06f5e0cdc231
rubenmachiels <ruben@reuring.studio>
2024-07-17 12:31:43 +0200

    Evelien Lulofs Snippets

--- public/spherestory/js/umc/pagescript.js
+++ public/spherestory/js/umc/pagescript.js
@@ -92,8 +92,8 @@ let viewer = pannellum.viewer("panorama", {
                     clickHandlerArgs: {hotspot: "sphere-hotspot_video-1", type: "video-modal"}
                 },
                 {
-                    pitch: 0.4,
-                    yaw: 41.29,
+                    pitch: -4.4,
+                    yaw: 43.29,
                     type: "info",
                     cssClass: "custom-hotspot sphere-hotspot_video-2",
                     clickHandlerFunc: hotspotAction,
