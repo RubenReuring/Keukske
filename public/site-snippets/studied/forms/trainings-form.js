2a215ad335eaf6e733f9629266f14acc04fb211f
rubenmachiels <ruben@reuring.studio>
2024-07-19 01:21:10 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/trainings-form.js
+++ public/site-snippets/studied/forms/trainings-form.js
@@ -140,8 +140,8 @@ $(document).ready(function() {
             $(document).on('change', 'input[name="lessons[]"]', function(){
                 $('.cc-fb_summarylist__content').empty();
                 $('input[name="lessons[]"]:checked').each(function(){
-                    let itemNumber = $(this).find('.p13-1-bold').text();
-                    let itemTitle = $(this).find('.p13-1-reg').text();
+                    let itemNumber = $(this).find('.checkboxblock-textwrap').find('.p13-1-bold').text();
+                    let itemTitle = $(this).find('.checkboxblock-textwrap').find('.p13-1-reg').text();
                     console.log(itemNumber,itemTitle)
                     var newSummaryRow = $(summaryRowTemplate);
                     newSummaryRow.children().eq(0).text(itemNumber);
