0cd05e68504637d6eab8dc667c604e3a0aeb309d
rubenmachiels <ruben@reuring.studio>
2024-07-18 13:07:35 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/trainings-form.js
+++ public/site-snippets/studied/forms/trainings-form.js
@@ -100,8 +100,8 @@ $(document).ready(function() {
 
         if($('#Volledig-programma').is(':checked') || $('#Verkort-programma').is(':checked')){
             if($('#Losse-lessen').is(':checked')){
-                numberSections();
                 $('.classes-selector').css('display', 'block');
+                numberSections();
             } else if(!$('#Losse-lessen').is(':checked')){
                 $('.classes-selector').css('display', 'none');
                 numberSections();
@@ -118,8 +118,8 @@ $(document).ready(function() {
     $('input[name="Programma"]').change(function() {
 
             if($('#Losse-lessen').is(':checked')){
-                numberSections();
                 $('.classes-selector').css('display', 'block');
+                numberSections();
             } else if(!$('#Losse-lessen').is(':checked')){
                 $('.classes-selector').css('display', 'none');
                 numberSections();
