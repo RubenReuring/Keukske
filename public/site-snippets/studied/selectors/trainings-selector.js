035b3b1a782499c8cd0791c113c4ace30680c84d
rubenmachiels <ruben@reuring.studio>
2024-08-12 22:31:58 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/selectors/trainings-selector.js
+++ public/site-snippets/studied/selectors/trainings-selector.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     console.log('Hi')
 
-    $('input[name="onderwijs-niveau"]').change(function(){
+    $('input[name="onderwijsniveau-radio"]').change(function(){
         if($(this).is(':checked')){
             console.log($(this))
         }
