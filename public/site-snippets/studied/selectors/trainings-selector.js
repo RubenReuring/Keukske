37c0f69461ab381611e9419c3bdb1417d5c1d370
rubenmachiels <ruben@reuring.studio>
2024-08-12 22:31:24 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/selectors/trainings-selector.js
+++ public/site-snippets/studied/selectors/trainings-selector.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
     console.log('Hi')
+
+    $('input[name="onderwijs-niveau"]').change(function(){
+        if($(this).is(':checked')){
+            console.log($(this))
+        }
+    });
+
 });
 
