3efaee6aeb2a271496f527717aa9ff4628a5575e
rubenmachiels <ruben@reuring.studio>
2024-08-12 22:40:44 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/selectors/trainings-selector.js
+++ public/site-snippets/studied/selectors/trainings-selector.js
@@ -10,6 +10,17 @@ $(document).ready(function(){
             currentDataTypeLink = $(this).siblings('.sfb-content__item-data').attr('href')
             console.log(currentDataTypeLink)
         }
+        var fullUrl = baseUrl + currentDataTypeLink;
+        $.ajax({
+            url: fullUrl,  // The URL to send the request to
+            method: 'GET', // The HTTP method to use (GET is the default)
+            success: function(data) {
+                console.log(data); // Log the response data to the console
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX request failed:', textStatus, errorThrown); // Log any errors to the console
+            }
+        });
     });
 
 });
