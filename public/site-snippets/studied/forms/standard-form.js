0455c5cdfc4faf15bb84186a0ac82e3cbc8cb558
rubenmachiels <ruben@reuring.studio>
2024-08-29 08:51:32 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/standard-form.js
+++ public/site-snippets/studied/forms/standard-form.js
@@ -5,8 +5,18 @@ $(document).ready(function() {
 
         if(isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'block')
+            $('#Aanhef---Ouder').attr('required');
+            $('#Voornaam---Ouder').attr('required');
+            $('#Achternaam---Ouder').attr('required');
+            ('#Mail---Ouder').attr('required');
+            ('#Telefoonr---Ouder').attr('required');
         } else if(!isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'none')
+            $('#Aanhef---Ouder').removeAttr('required');
+            $('#Voornaam---Ouder').removeAttr('required');
+            $('#Achternaam---Ouder').removeAttr('required');
+            ('#Mail---Ouder').removeAttr('required');
+            ('#Telefoonr---Ouder').removeAttr('required');
         }
 
     });
