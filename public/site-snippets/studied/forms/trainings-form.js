99af04d62ec1a13efede159107cb54f4df36ad62
rubenmachiels <ruben@reuring.studio>
2024-08-29 08:34:37 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/trainings-form.js
+++ public/site-snippets/studied/forms/trainings-form.js
@@ -226,4 +226,11 @@ $(document).ready(function() {
         });
     });
 
+
+    // Minderjarigheidscontrole
+    $('input[name="Minderjarig"]').change(function() {
+        let currentVal = $(this).val()
+        console.log(currentVal)
+    });
+
 });
