e5418dc015c23e322aea8643e86e054b4699663b
rubenmachiels <ruben@reuring.studio>
2024-08-29 08:40:30 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/standard-form.js
+++ public/site-snippets/studied/forms/standard-form.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
-    // Minderjarigheidscontrole
     $('input[name="Minderjarig"]').change(function () {
-        let currentVal = $(this).val()
-        console.log(currentVal)
+        let isChecked = $(this).prop('checked');
+        console.log(isChecked);
     });
 
 });
