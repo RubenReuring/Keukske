8c296d068fa4130d26e539da177b0303aba85511
rubenmachiels <ruben@reuring.studio>
2024-08-29 08:48:42 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/standard-form.js
+++ public/site-snippets/studied/forms/standard-form.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 
     $('input[name="Minderjarig"]').change(function () {
         let isChecked = $(this).prop('checked');
-        console.log(isChecked);
+
+        if(isChecked){
+            $('.cc-fb_wrap.form-parental').css('display', 'block')
+        } else if(!isChecked){
+            $('.cc-fb_wrap.form-parental').css('display', 'none')
+        }
+
     });
 
 });
