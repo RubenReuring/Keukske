701a73798a79bd8418fc07578ae798c40a5168e9
rubenmachiels <ruben@reuring.studio>
2024-09-02 17:04:26 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/trainings-form.js
+++ public/site-snippets/studied/forms/trainings-form.js
@@ -226,4 +226,25 @@ $(document).ready(function() {
         });
     });
 
+    $('input[name="Minderjarig"]').change(function () {
+        let isChecked = $(this).prop('checked');
+
+        if(isChecked){
+            $('.cc-fb_wrap.form-parental').css('display', 'block')
+            $('#Aanhef---Ouder').attr('required', 'required');
+            $('#Voornaam---Ouder').attr('required', 'required');
+            $('#Achternaam---Ouder').attr('required', 'required');
+            $('#Mail---Ouder').attr('required', 'required');
+            $('#Telefoonr---Ouder').attr('required', 'required');
+        } else if(!isChecked){
+            $('.cc-fb_wrap.form-parental').css('display', 'none')
+            $('#Aanhef---Ouder').removeAttr('required');
+            $('#Voornaam---Ouder').removeAttr('required');
+            $('#Achternaam---Ouder').removeAttr('required');
+            $('#Mail---Ouder').removeAttr('required');
+            $('#Telefoonr---Ouder').removeAttr('required');
+        }
+
+    });
+
 });
