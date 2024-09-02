16d28c61c26499cb310c2fdf0e7257aa33b9e28d
rubenmachiels <ruben@reuring.studio>
2024-09-02 16:58:08 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/standard-form.js
+++ public/site-snippets/studied/forms/standard-form.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 
         if(isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'block')
-            $('#Aanhef---Ouder').attr('required');
-            $('#Voornaam---Ouder').attr('required');
-            $('#Achternaam---Ouder').attr('required');
-            $('#Mail---Ouder').attr('required');
-            $('#Telefoonr---Ouder').attr('required');
+            $('#Aanhef---Ouder').attr('required', 'required');
+            $('#Voornaam---Ouder').attr('required', 'required');
+            $('#Achternaam---Ouder').attr('required', 'required');
+            $('#Mail---Ouder').attr('required', 'required');
+            $('#Telefoonr---Ouder').attr('required', 'required');
         } else if(!isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'none')
             $('#Aanhef---Ouder').removeAttr('required');
