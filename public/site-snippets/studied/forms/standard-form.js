00be59b4db834d160d8309f2131fe2c193d07dfd
rubenmachiels <ruben@reuring.studio>
2024-09-02 16:57:21 +0200

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/standard-form.js
+++ public/site-snippets/studied/forms/standard-form.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
 
         if(isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'block')
-            $('#Aanhef---Ouder').attr('required', 'required');
-            $('#Voornaam---Ouder').attr('required', 'required');
-            $('#Achternaam---Ouder').attr('required', 'required');
-            ('#Mail---Ouder').attr('required', 'required');
-            ('#Telefoonr---Ouder').attr('required', 'required');
+            $('#Aanhef---Ouder').attr('required');
+            $('#Voornaam---Ouder').attr('required');
+            $('#Achternaam---Ouder').attr('required');
+            $('#Mail---Ouder').attr('required');
+            $('#Telefoonr---Ouder').attr('required');
         } else if(!isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'none')
             $('#Aanhef---Ouder').removeAttr('required');
             $('#Voornaam---Ouder').removeAttr('required');
             $('#Achternaam---Ouder').removeAttr('required');
-            ('#Mail---Ouder').removeAttr('required');
-            ('#Telefoonr---Ouder').removeAttr('required');
+            $('#Mail---Ouder').removeAttr('required');
+            $('#Telefoonr---Ouder').removeAttr('required');
         }
 
     });
