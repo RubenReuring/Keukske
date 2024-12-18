85a7b616607f5dd5cc8c8c56d23bb2d9d91aebbf
rubenmachiels <ruben@reuring.studio>
2024-12-18 18:31:01 +0100

    Evelien Lulofs Snippets

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,20 +11,50 @@ class WebhookController extends Controller
     public function handleInstagram(Request $request)
     {
         $signature = $request->header('X-Webflow-Signature');
-        Log::info('X-Webflow-Signature:', ['signature' => $signature]);
-        return response()->json(['message' => 'Instagram signature logged.']);
+        $validSignatures = [
+            'cabdc5b4d50bd30ba707db1dbf01ad7530ac8d1c036fcac9458c227015cbaed7',
+            'bb57153b0f09fc0a3118839dd760dfb54ce027bfa8bb8cafb70fc55c5f4c3a3c',
+        ];
+
+        if (in_array($signature, $validSignatures)) {
+            Log::info('Valid Webhook Signature:', ['signature' => $signature]);
+            return response()->json(['message' => 'Valid signature.']);
+        }
+
+        Log::warning('Invalid Webhook Signature:', ['signature' => $signature]);
+        return response()->json(['error' => 'Invalid signature.'], 403);
     }
     public function handleFacebook(Request $request)
     {
         $signature = $request->header('X-Webflow-Signature');
-        Log::info('X-Webflow-Signature:', ['signature' => $signature]);
-        return response()->json(['message' => 'Facebook signature logged.']);
+        $validSignatures = [
+            'cabdc5b4d50bd30ba707db1dbf01ad7530ac8d1c036fcac9458c227015cbaed7',
+            'bb57153b0f09fc0a3118839dd760dfb54ce027bfa8bb8cafb70fc55c5f4c3a3c',
+        ];
+
+        if (in_array($signature, $validSignatures)) {
+            Log::info('Valid Webhook Signature:', ['signature' => $signature]);
+            return response()->json(['message' => 'Valid signature.']);
+        }
+
+        Log::warning('Invalid Webhook Signature:', ['signature' => $signature]);
+        return response()->json(['error' => 'Invalid signature.'], 403);
     }
     public function handleLinkedin(Request $request)
     {
         $signature = $request->header('X-Webflow-Signature');
-        Log::info('X-Webflow-Signature:', ['signature' => $signature]);
-        return response()->json(['message' => 'Linkedin signature logged.']);
+        $validSignatures = [
+            'cabdc5b4d50bd30ba707db1dbf01ad7530ac8d1c036fcac9458c227015cbaed7',
+            'bb57153b0f09fc0a3118839dd760dfb54ce027bfa8bb8cafb70fc55c5f4c3a3c',
+        ];
+
+        if (in_array($signature, $validSignatures)) {
+            Log::info('Valid Webhook Signature:', ['signature' => $signature]);
+            return response()->json(['message' => 'Valid signature.']);
+        }
+
+        Log::warning('Invalid Webhook Signature:', ['signature' => $signature]);
+        return response()->json(['error' => 'Invalid signature.'], 403);
 
     }
 }
