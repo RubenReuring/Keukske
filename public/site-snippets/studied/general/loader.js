6909a9dec2b8e2f68484b2c0e9b4dd076eda63dc
rubenmachiels <ruben@reuring.studio>
2025-01-02 16:31:22 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -1,4 +1,8 @@
+gsap.RegisterPlugin(SplitText);
+
 let heroTitleSplit = new SplitText($(".pli-bottom__text").find("p"), {
     type: "lines",
     linesClass: "ls-line-mask",
 });
+
+$(".pli-bottom__text").find("p").children("div").wrapInner('<span style="display: block; text-align: center; position: relative;"></span>');
