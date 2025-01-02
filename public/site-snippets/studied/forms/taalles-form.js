48b0de6b9dbc1423b630a0349d8f456e04313d86
rubenmachiels <ruben@reuring.studio>
2025-01-02 11:49:43 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/forms/taalles-form.js
+++ public/site-snippets/studied/forms/taalles-form.js
@@ -5,14 +5,12 @@ $(document).ready(function() {
 
         if(isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'block')
-            $('#Aanhef---Ouder').attr('required', 'required');
             $('#Voornaam---Ouder').attr('required', 'required');
             $('#Achternaam---Ouder').attr('required', 'required');
             $('#Mail---Ouder').attr('required', 'required');
             $('#Telefoonr---Ouder').attr('required', 'required');
         } else if(!isChecked){
             $('.cc-fb_wrap.form-parental').css('display', 'none')
-            $('#Aanhef---Ouder').removeAttr('required');
             $('#Voornaam---Ouder').removeAttr('required');
             $('#Achternaam---Ouder').removeAttr('required');
             $('#Mail---Ouder').removeAttr('required');
