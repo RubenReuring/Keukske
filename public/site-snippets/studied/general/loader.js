d207e5a2cd96545087d05f80793cadc3f7a4707f
rubenmachiels <ruben@reuring.studio>
2025-01-02 16:52:08 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -1,9 +1,9 @@
 let heroTitleSplit = new SplitText($(".pli-bottom__text").find("p"), {
-    type: "lines",
+    type: "words",
     linesClass: "ls-line-mask",
 });
 
-$(".pli-bottom__text").find("p").children("div").wrapInner('<span style="display: block; position: relative;"></span>');
+$(".pli-bottom__text").find("p").children("div").wrapInner('<span style="display: block; text-align: center; position: relative;"></span>');
 
 let loaderTimeline = gsap.timeline();
 let loaderSloganTargets = gsap.utils.toArray(
