eed2117b06fa8db58a33f3063955bd5783bd0641
rubenmachiels <ruben@reuring.studio>
2025-01-02 17:06:28 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -48,12 +48,12 @@ loaderTimeline.fromTo(
 loaderTimeline.fromTo(
     '.page-loader__bg',
     {y: "0%"},
-    {y: "-100%", duration: .7, ease: "power2.out"},
+    {y: "-100%", duration: .7, ease: "power2.inOut"},
     3.3
 );
 loaderTimeline.fromTo(
     '.page-loader',
     {y: "0%"},
-    {y: "-100%", duration: .7, ease: "power2.out"},
+    {y: "-100%", duration: .7, ease: "power2.inOut"},
     3.35
 );
