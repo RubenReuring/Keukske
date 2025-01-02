f7ce03acbcd9d1e5b293169cd6cbb406ffbcd00d
rubenmachiels <ruben@reuring.studio>
2025-01-02 16:32:54 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -4,3 +4,20 @@ let heroTitleSplit = new SplitText($(".pli-bottom__text").find("p"), {
 });
 
 $(".pli-bottom__text").find("p").children("div").wrapInner('<span style="display: block; position: relative;"></span>');
+
+let loaderTimeline = gsap.timeline();
+let loaderSloganTargets = gsap.utils.toArray(
+    $(".ls-inner").find('span')
+);
+
+loaderTimeline.fromTo(
+    '.loader',
+    {backgroundColor: "#ffffff"},
+    {backgroundColor: "#fe9963", duration: .75, ease: "power1.inOut"}
+);
+loaderTimeline.fromTo(
+    loaderSloganTargets,
+    {y: "125%"},
+    {y: "0%", stagger: 0.15, duration: 1.35, ease: "power3.out"},
+    .4
+);
