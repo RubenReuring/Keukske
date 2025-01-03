3439c179b2ee8b587f3038997741ae0387c8ef3e
rubenmachiels <ruben@reuring.studio>
2025-01-03 15:11:24 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/general.js
+++ public/site-snippets/studied/general/general.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
     let navbarTransitionLarge = gsap.timeline({ paused: true });
     navbarTransitionLarge.fromTo(
         '.nav',
-        { backgroundColor: 'rgba(245, 242, 235, 0)' },
-        { backgroundColor: 'rgba(245, 242, 235, 1)', duration: 0.55, ease: "power2.inOut" }
+        { backgroundColor: 'rgba(245, 242, 235, 0)', className: "+=nav-at-top" },
+        { backgroundColor: 'rgba(245, 242, 235, 1)', className: "-=nav-at-top",  duration: 0.55, ease: "power2.inOut" }
     );
 
     // GSAP timeline for 991px and below (no paddingTop change)
