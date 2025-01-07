1eed2fc0cc7a5e321c5303c225415adfb3b34861
rubenmachiels <ruben@reuring.studio>
2025-01-07 20:05:51 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -30,14 +30,14 @@ loaderTimeline.fromTo(
 loaderTimeline.fromTo(
     '.pli-pulse',
     {opacity: "1", width: '20em', height: '20em'},
-    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.25, ease: "power1.out"},
+    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.4, ease: "power2.out"},
     1.75
 );
 loaderTimeline.fromTo(
     '.pli-pulse',
     {opacity: "1", width: '20em', height: '20em'},
-    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.25, ease: "power1.out"},
-    2.75
+    {opacity: "0", width: '30em', height: '30em', duration: 1.4, ease: "power2.out"},
+    3
 );
 loaderTimeline.fromTo(
     '.pli-inner',
