fcc79c90ae401f3d66934f1e3d3097638df527b7
rubenmachiels <ruben@reuring.studio>
2025-01-07 20:03:30 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -30,14 +30,14 @@ loaderTimeline.fromTo(
 loaderTimeline.fromTo(
     '.pli-pulse',
     {opacity: "1", width: '20em', height: '20em'},
-    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.2, ease: "power1.out"},
+    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.25, ease: "power1.out"},
     1.75
 );
 loaderTimeline.fromTo(
     '.pli-pulse',
     {opacity: "1", width: '20em', height: '20em'},
-    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.2, ease: "power1.out"},
-    2.5
+    {opacity: "0", width: '27.5em', height: '27.5em', duration: 1.25, ease: "power1.out"},
+    2.75
 );
 loaderTimeline.fromTo(
     '.pli-inner',
