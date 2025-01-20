1ebad41d50078f00f30323c3bbf2709fa7104d1d
rubenmachiels <ruben@reuring.studio>
2025-01-20 11:53:33 +0100

    Evelien Lulofs Snippets

--- public/site-snippets/studied/general/loader.js
+++ public/site-snippets/studied/general/loader.js
@@ -46,11 +46,11 @@
 
 /////////////////// PAGE LOADER V2 ///////////////////
 let loaderTimeline = gsap.timeline();
-loaderTimeline.fromTo( '.pli-bottom__visual',   {opacity: "0"},  {opacity: "1", duration: .3, ease: "power3.out"});
-loaderTimeline.fromTo( '.pli-pulse',            {opacity: "1", width: '17.5em', height: '17.5em'}, {opacity: "0", width: '25em', height: '25em', duration: 1.25, ease: "power1.out"}, .2 );
-// loaderTimeline.fromTo( '.pli-pulse',            {opacity: "1", width: '17.5em', height: '17.5em'}, {opacity: "0", width: '25em', height: '25em', duration: 1.25, ease: "power1.out"}, 1.25 );
-loaderTimeline.fromTo( '.page-loader__bg',      {y: "0%"}, {y: "-100%", duration: .6, ease: "power2.inOut"}, 1.3);
-loaderTimeline.fromTo( '.page-loader',          {y: "0%"}, {y: "-100%", duration: .6, ease: "power2.inOut"}, 1.4 );
+loaderTimeline.fromTo( '.pli-bottom__visual',   {opacity: "0"},  {opacity: "1", duration: .3, ease: "power1.out"});
+loaderTimeline.fromTo( '.pli-pulse',            {opacity: "1", width: '17.5em', height: '17.5em'}, {opacity: "0", width: '25em', height: '25em', duration: 1.1, ease: "power1.out"}, .1 );
+loaderTimeline.fromTo( '.pli-pulse',            {opacity: "1", width: '17.5em', height: '17.5em'}, {opacity: "0", width: '25em', height: '25em', duration: 1.1, ease: "power1.out"}, 1 );
+loaderTimeline.fromTo( '.page-loader__bg',      {y: "0%"}, {y: "-100%", duration: .6, ease: "power1.inOut"}, .9 );
+loaderTimeline.fromTo( '.page-loader',          {y: "0%"}, {y: "-100%", duration: .6, ease: "power1.inOut"}, 1 );
 loaderTimeline.to( '.pli-bottom__visual',       {opacity: "0", duration: 0, ease: "power3.out"}, 2.5);
 
 $('a').on('click', function (e) {
